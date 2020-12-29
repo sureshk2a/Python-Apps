f459be4e0c2e519140fd9896b5ab4c700330106d
Suresh Kumar <34769206+sureshk2a@users.noreply.github.com>
2020-12-29 14:39:36 +0530

    Dictionary initial commit

---
+++ CLI_Dictionary/app.py
@@ -0,0 +1,26 @@
+import json
+from difflib import SequenceMatcher,get_close_matches
+
+with open('CLI_Dictionary\data.json') as f:
+  data = json.load(f)
+
+def getCloseMatch(userKey):
+    # for word in get_close_matches(userKey,data.keys(),cutoff=0.8):
+    #     print(word)
+    return get_close_matches(userKey,data.keys(),cutoff=0.8)
+
+def getValue(userKey):
+    closeMatches = getCloseMatch(userKey)
+    result = data.get(closeMatches[0])
+
+    if userKey in data:
+        print(data.get(userKey))
+    elif len(closeMatches) > 0:
+        choice = input("Did you mean '{0}'?, If yes press 'y' else 'n'".format(closeMatches[0]))
+        if choice == "y": print(str(result))
+        else: print("Sorry the word you searching for may be incorrect, please check")
+
+
+# userKey = input("Enter the string to search in the dictionary: \n")
+getValue("tigerr")#userKey.lower())
+
